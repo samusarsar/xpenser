2f1c3d6e9b7d5d3ada9c1ee1e78f74ac51e85469
ssarandev <samuil.sarandev@dxc.com>
2023-12-15 10:47:41 +0200

    Add: add dummy components for DashboardView

---
+++ src/components/dashboard/Header.vue
@@ -0,0 +1,8 @@
+<template>
+  <h2>Xpenser</h2>
+  <h3>The New Expense Tracker</h3>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
---
+++ src/views/DashboardView.vue
@@ -0,0 +1,19 @@
+<template>
+  <Header />
+  <div class="container">
+    <Balance />
+    <IncomeExpenses />
+    <TransactionList />
+    <AddTransaction />
+  </div>
+</template>
+
+<script setup>
+import Header from '../components/dashboard/Header.vue'
+import Balance from '../components/dashboard/Balance.vue'
+import IncomeExpenses from '../components/dashboard/IncomeExpenses.vue'
+import TransactionList from '../components/dashboard/TransactionList.vue'
+import AddTransaction from '../components/dashboard/AddTransaction.vue'
+</script>
+
+<style scoped></style>
